52d8ea000ea9b0da8c02ee4bf152e6c65e1b77d0
bobbyquennell <bobbyquennell@gmail.com>
2017-10-23 02:25:02 +1100

    update

--- validator.Domain/Feature/BruteTryProtector.cs
+++ validator.Domain/Feature/BruteTryProtector.cs
@@ -14,23 +14,29 @@ namespace validator.Domain.Feature
 
         public static void CreateTfnPool()
         {
-            tfns = new Dictionary<string,TFNtoCheck>();
+            tfns = new Dictionary<string, TFNtoCheck>();
         }
         public static bool IfBruteAttack(int newTfn)
         {
+
+            //List<string> conNumGroup= ConNumberParser.findConsecutiveNumbers(newTfn.ToString(), 4);
+
+
+
             var tfn = new TFNtoCheck
             {
                 Value = newTfn,
                 TimeStamp = DateTime.Now
             };
-            if(tfns.Count == 0)
+            if (tfns.Count == 0)
             {
                 tfns.Add(newTfn.ToString(), tfn);
             }
             else
             {
                 RemoveOldTfn(tfn);
-                if (!tfns.ContainsKey(newTfn.ToString())){
+                if (!tfns.ContainsKey(newTfn.ToString()))
+                {
                     tfns.Add(newTfn.ToString(), tfn);
                 }
             }
@@ -50,6 +56,81 @@ namespace validator.Domain.Feature
                     tfns.Remove(list[i].Value.ToString());
                 }
             };
+        }
+
+
+    }
+
+    public class ConNumberParser
+    {
+
+        public static List<String> findConsecutiveNumbers(String s, int length)
+        {
+            List<String> matches = new List<String>();
+
+            // seek to first digit
+            int i = 0;
+            while (i < s.Length && !Char.IsDigit(s[i]))
+            {
+                i++;
+            }
+            // store the beginning of a consecutive series
+            int? matchedIndex = i;
+            // and the direction
+            Boolean increasing = false;
+
+            for (i=0; i < s.Length; i++)
+            {
+                Char c = s[i];
+                if (Char.IsDigit(c))
+                {
+                    if (null == matchedIndex)
+                    {
+                        // first digit after other characters
+                        matchedIndex = i;
+                        increasing = false;
+                        continue;
+                    }
+                    int difference = Convert.ToInt32(c) - Convert.ToInt32(s[i - 1]);
+                    if (Math.Abs(difference) > 1)
+                    {
+                        // no conescutive digits
+                        matchedIndex = i;
+                        increasing = false;
+                        continue;
+                    }
+                    if (length > 2)
+                    {
+                        if (false == increasing)
+                        {
+                            // found first consecutive digit
+                            increasing = (difference == 1);
+                            continue;
+                        }
+                        int expectedDiff = increasing ? 1 : -1;
+                        if (difference != expectedDiff)
+                        {
+                            // no conescutive digits in the right direction
+                            matchedIndex = i - 1;
+                            increasing = (difference == 1);
+                            continue;
+                        }
+                    }
+                    if (i - matchedIndex + 1 == length)
+                    {
+                        // consecutive digits of given length found
+                        matches.Add(s.Substring((int)matchedIndex, (int)matchedIndex + length));
+                        matchedIndex++; // move by one to keep matching overlapping
+                                        // sequences
+                    }
+                }
+                else
+                {
+                    matchedIndex = null;
+                }
+            }
+
+            return matches;
         }
     }
 }
